66ee9c71ea60c13e1c1aa6213789730874178e8f
gitkrak <dpemdnjem23@gmail.com>
2024-10-05 00:13:48 +0900

    퍼포먼스 테스트 추가

---
+++ react-performance-test/src/testFolder/PerformanceTest.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import PhotoOne from "./photoOne";
+import PhotoTwo from "./photoTwo";
+
+const PerformanceTest = () => {
+  const [message, setMessage] = React.useState("");
+  const [photos, setPhotos] = React.useState([]);
+
+ 
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default PerformanceTest;
